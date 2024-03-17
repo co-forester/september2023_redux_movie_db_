42a6b30b89c41e5ed807751c7d23256b8f14cd58
SerGio <foresterares@gmail.com>
2024-03-17 10:36:49 +0200

    created const handleInputChange

--- src/components/MoviesContainer/Search/Search.tsx
+++ src/components/MoviesContainer/Search/Search.tsx
@@ -1,36 +1,38 @@
-import React, {useEffect} from 'react';
+import React, {ChangeEvent, useEffect} from 'react';
+import {useSearchParams} from "react-router-dom";
 
 import css from './Search.module.css'
 import {useAppDispatch, useAppSelector} from "../../../hooks";
-import {useSearchParams} from "react-router-dom";
 import {movieActions} from "../../../Redux";
 import {Movies} from "../Movies";
 
 const Search = () => {
     const {movies} = useAppSelector(state => state.movies);
-    const [query, setQuery] = useSearchParams({query: 'star', page:'1'});
+    const [query, setQuery] = useSearchParams({query: '', page:'1'});
     const dispatch = useAppDispatch();
     const page = + query.get('page');
-    const keyWord = query.get('query')
+    const keyWord = query.get('query');
 
     useEffect(() => {
         dispatch(movieActions.getKeyWord({keyWord, page}))
-    }, [keyWord, page]);
+    }, [keyWord, page, dispatch]);
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const query = event.target.value;
+        setQuery({ query, page: '1' }); // Оновлюємо значення параметра "word" у `query`
+    };
 
     return (
             <div className={css.Search}>
                 <div className={css.SearchHead}>
-                    <div className={css.SearchMap}>
-                        <div>
-                            <form>
-                                <input type={"text"} placeholder={'search'} className={css.SearchInput}
-                                       onChange={(event) => setQuery({query: event.target.value})}
+                            <form className={css.SearchMap}>
+                                <input
+                                    type={"text"} placeholder={'search'} className={css.SearchInput}
+                                    value={keyWord || ''}
+                                    onChange={handleInputChange}
                                 />
                                 {/*<img src={} alt={}>*/}
                             </form>
-                        </div>
-                    </div>
-
                 </div>
                 <Movies movies={movies}/>
             </div>
