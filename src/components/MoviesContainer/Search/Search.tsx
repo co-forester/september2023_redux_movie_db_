e6a927bf3f7288c8d8d215300f5fad85f9097343
SerGio <foresterares@gmail.com>
2024-03-17 10:38:25 +0200

    created const handleInputChange

--- src/components/MoviesContainer/Search/Search.tsx
+++ src/components/MoviesContainer/Search/Search.tsx
@@ -8,9 +8,9 @@ import {Movies} from "../Movies";
 
 const Search = () => {
     const {movies} = useAppSelector(state => state.movies);
-    const [query, setQuery] = useSearchParams({query: '', page:'1'});
+    const [query, setQuery] = useSearchParams({query: '', page: '1'});
     const dispatch = useAppDispatch();
-    const page = + query.get('page');
+    const page = +query.get('page');
     const keyWord = query.get('query');
 
     useEffect(() => {
@@ -19,23 +19,23 @@ const Search = () => {
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
         const query = event.target.value;
-        setQuery({ query, page: '1' }); // Оновлюємо значення параметра "word" у `query`
+        setQuery({query, page: '1'}); // Оновлюємо значення параметра "word" у `query`
     };
 
     return (
-            <div className={css.Search}>
-                <div className={css.SearchHead}>
-                            <form className={css.SearchMap}>
-                                <input
-                                    type={"text"} placeholder={'search'} className={css.SearchInput}
-                                    value={keyWord || ''}
-                                    onChange={handleInputChange}
-                                />
-                                {/*<img src={} alt={}>*/}
-                            </form>
-                </div>
-                <Movies movies={movies}/>
+        <div className={css.Search}>
+            <div className={css.SearchHead}>
+                <form className={css.SearchMap}>
+                    <input
+                        type={"text"} placeholder={'search'} className={css.SearchInput}
+                        value={keyWord || ''}
+                        onChange={handleInputChange}
+                    />
+                    {/*<img src={} alt={}>*/}
+                </form>
             </div>
+            <Movies movies={movies}/>
+        </div>
     );
 };
 
